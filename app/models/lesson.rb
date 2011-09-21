776da4b28e4950a78127be3dd7caace8f383d31a
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-09-21 12:26:38 -0700

    more confidence score options

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,10 +1,12 @@
 class Lesson < ActiveRecord::Base
 
   CONFIDENCE_SCORES = [
-    ["Little to no experience, basically no idea what I'm doing.", 0],
-    ["Some experience, starting to get the hang of things.", 10],
-    ["Solid experience, not an expert, but I know what I'm doing", 20],
-    ["Expert, I've been doing this for years", 30]
+    ["Just getting started, basically no idea what I'm doing.", 0],
+    ["A little experience, I'm not totally clueless.", 10],
+    ["Some experience, starting to get the hang of things.", 20],
+    ["Novice, I've got a long way to go, but I'm feeling comfortable.", 30],
+    ["Solid experience, not an expert, but I know what I'm doing", 40],
+    ["Expert, I've been doing this for years", 50]
   ]
 
 end
