fb376ecf369a7580e0e719e8328b46279fb4c8e4
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-09-23 10:51:32 -0700

    Starting Over

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Upsidedownacademy::Application.config.secret_token = 'aef09e1d89bcae1f0100e2e7e9f0da5a76d1161106571cf859157a3f933ad1af456ba388fe30114cffb0d7365501596db4c26372c2225e2cfe16d15bf3e3b72f'
