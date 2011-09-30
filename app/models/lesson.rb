11f75804d686582e32b3ad69c50eb182f5303bde
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-09-30 09:54:27 -0700

    - tweaking confidence levels

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,14 +5,13 @@ class Lesson < ActiveRecord::Base
 
   CONFIDENCE_SCORES = [
     ["Just getting started, basically no idea what I'm doing.", 0],
-    ["A little experience, I'm kind of figuring it out", 10],
-    ["A little confused, I thought I was getting it, but now I'm not so sure.", 20],
-    ["Novice, I've got a long way to go, but I'm feeling comfortable.", 50],
-    ["Some experience, starting to get the hang of things.", 30],
-    ["Feeling frustrated, I should be progressing better right now.", 40],
-    ["Solid experience, not an expert, but I know what I'm doing.", 60],
-    ["Expert, I know this in and out.. I won't lead you astray.", 70],
-    ["Beyond an expert. Experts look up to me.", 80]
+    ["A little experience, I'm kind of figuring it out.", 30],
+    ["Novice, I've got a long way to go, but I'm feeling reasonably comfortable.", 60],
+    ["Some experience, starting to get the hang of things.", 90],
+    ["Good experience, probably know more than most people.", 120],
+    ["Solid experience, not an expert, but I know what I'm doing.", 150],
+    ["Expert, I know this in and out.. I won't lead you astray.", 180],
+    ["Beyond an expert. Experts look up to me.", 210]
   ]
 
   def full_title
@@ -24,6 +23,6 @@ class Lesson < ActiveRecord::Base
   end
 
   def confidence_text
-    CONFIDENCE_SCORES.detect { |score_info| score_info[1] == confidence_score }[0]
+    (CONFIDENCE_SCORES.detect { |score_info| score_info[1] == confidence_score } || [])[0]
   end
 end
