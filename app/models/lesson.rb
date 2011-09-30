2db0c1e64512d3661bc32dc85083a04213a6b1a2
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-09-30 10:36:59 -0700

    - tweaking confidence levels

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,13 +5,16 @@ class Lesson < ActiveRecord::Base
 
   CONFIDENCE_SCORES = [
     ["Just getting started, basically no idea what I'm doing.", 0],
-    ["A little experience, I'm kind of figuring it out.", 30],
-    ["Novice, I've got a long way to go, but I'm feeling reasonably comfortable.", 60],
-    ["Some experience, starting to get the hang of things.", 90],
-    ["Good experience, probably know more than most people.", 120],
-    ["Solid experience, not an expert, but I know what I'm doing.", 150],
-    ["Expert, I know this in and out.. I won't lead you astray.", 180],
-    ["Beyond an expert. Experts look up to me.", 210]
+    ["Not clueless, it's no longer a completely foreign language.", 10],
+    ["A little experience, I'm kind of figuring it out.", 20],
+    ["Novice, I've got a long way to go, but I'm starting to feel comfortable.", 30],
+    ["Some experience, getting the hang of things.", 40],
+    ["I'm probably in the middle of the pack. I've got a decent idea what I'm doing.", 50],        
+    ["Good experience, probably know more than most people.", 60],
+    ["Solid experience, not an expert, but I know what I'm doing.", 70],
+    ["Probably not an expert yet, but I'm close. I've got a very good understanding.", 80],
+    ["Expert, I know this in and out.. I won't lead you astray.", 90],
+    ["Beyond an expert. Experts look up to me.", 100]
   ]
 
   def full_title
@@ -23,6 +26,7 @@ class Lesson < ActiveRecord::Base
   end
 
   def confidence_text
-    (CONFIDENCE_SCORES.detect { |score_info| score_info[1] == confidence_score } || [])[0]
+    score = CONFIDENCE_SCORES.detect { |score_info| score_info[1] == confidence_score }
+    score.nil? ? "" : "#{score[1]}: #{score[0]}"
   end
 end
