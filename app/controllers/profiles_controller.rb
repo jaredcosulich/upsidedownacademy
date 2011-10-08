6b6cabe13ab90a8ea780933215ca94622a7054b0
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-10-08 11:23:49 -0700

    - user profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
 
   def show
-    @user = User.find_by_id(Integer.unobfuscate(params[:id]))
+    @user = User.with_lessons.find_by_id(Integer.unobfuscate(params[:id]))
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :nickname, :email, :password, :password_confirmation, :remember_me
 
+  scope :with_lessons, includes(:lessons).where("lessons.published_at is not null")
+
   def display_name
     nickname || name
   end
