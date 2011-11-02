a28ade3334cfcdf4e575b7b63959c812c2d6b6f1
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2011-11-02 14:21:38 -0700

    - fixing user lesson list

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -2,7 +2,7 @@ class MyController < ApplicationController
   before_filter :authenticate_user!
 
   def lessons
-    @lessons = current_user.lessons
+    @lessons = current_user.lessons.published.recent
   end
 
 end
