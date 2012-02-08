9784d4869e8b1f20477951130f8c96878e0f4d4f
Jared Cosulich <jaredcosulich@Jared-Cosulichs-MacBook-Pro.local>
2012-02-07 16:04:50 -0800

    - embedding an already uploaded youtube video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,9 +1,16 @@
 class Video < ActiveRecord::Base
   belongs_to :lesson
 
+  before_save :convert_youtube_url_to_id
+  attr_accessor :youtube_url
+
   def setup_youtube(lesson_url)
     YouTube.post_video_info(self, lesson_url)
   end
   
+  def convert_youtube_url_to_id
+    return if youtube_url.blank? || youtube_id.present?
+    self.youtube_id = youtube_url.gsub('http://youtu.be/', '')
+  end
 
 end
